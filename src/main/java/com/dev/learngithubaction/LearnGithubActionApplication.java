73797fa171a4c59db54973ddab9f724a975e32f5
euncheol-kim <klmeuncheol@kakao.com>
2025-06-15 13:29:33 +0900

    setting, pure-gradle-ci

---
+++ src/main/java/com/dev/learngithubaction/LearnGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.dev.learngithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/learngithubaction/LearnGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.learngithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
